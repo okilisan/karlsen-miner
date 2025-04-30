4484dc45c883d801089fc84dc2b4e8d3d1b74ec2
okilisan <github@karlsencoin.com>
2025-05-01 00:18:41 +0200

    GPU DAG generation + workload lock removed

---
+++ run_miner_devnet_remote.sh
@@ -0,0 +1,7 @@
+./target/release/karlsen-miner.exe \
+    -s 198.27.75.193 \
+    -p 42610 \
+    --devnet \
+    --mining-address karlsendev:qprsjk60rc6qdlngragh9fu2nqzph40de5vz85z55gn2l57q8ufc5a2s2940s \
+    --mine-when-not-synced  \
+    --cuda-workload 10 \
